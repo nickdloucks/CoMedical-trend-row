83d7c93010b9c7db7b6c78f1e846c2b81c0b45ad
Nick <nickdloucks@outlook.com>
2022-06-29 21:37:45 -0700

    add more fields

--- src/components/zip-code-row.tsx
+++ src/components/zip-code-row.tsx
@@ -3,7 +3,10 @@ import React, { Fragment } from "react";
 interface Row{
     customer: string,
     zip: number,
-
+    product_group_id: string,
+    item: string,
+    quantity: number,
+    ext_sales: number
 }
 
 
