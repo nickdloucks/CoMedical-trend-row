06da4d52f3459cfc7eb147d81e60f940fb203075
Nick <80440799+nickdloucks@users.noreply.github.com>
2022-07-08 09:19:15 -0700

    comments

--- src/components/zip-code-row.tsx
+++ src/components/zip-code-row.tsx
@@ -1,6 +1,12 @@
 import React, { Fragment } from "react";
 
-interface Row{
+//This component will organize customers/sales data by zip code
+// So that the direct sales do not need to be linked to a ship-to ID
+// (With thousands of sales and hundreds of ship-tos, and with each manufacturer using different naming conventions,
+// This was the best solution so far 
+
+
+interface Row{ // a row represents all the data for one sale from the sales tracings
     customer: string,
     zip: number,
     product_group_id: string,
